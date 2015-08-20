81dea44ba3c8089b40b2659da311889d31ae260c
Kumaripaba <paba50@gmail.com>
2015-08-20 14:02:16 +0300

    edited the traininig_model to remember the position of visited/read/clicked items, and add [rank,query words, title length] as one items - all together 63 items per state

--- Train Search Model/main.m
+++ Train Search Model/main.m
@@ -7,7 +7,8 @@ order = 1; %order =1 -> descending order of relevance,
                %order = 3 -> totally random relevance distribution
 
 first_run=1; % 1 first run; 0 not firstrun -> if testing
-max_time = 60000; 
+max_time = 30000;
+%reset it to 30000 considering half a minute
 %set it to 60 seconds - as prposed by Antti
 % max_time is the maximum amount of time allocated per 
                   %search task in milliseconds. For now we set it to 5
